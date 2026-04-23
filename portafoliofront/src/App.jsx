abedcf6c77698d3e4ed5515aafd3c72b497cb9c7
loboxos <vcristian528@gmail.com>
2026-04-23 13:49:59 -0300

    config mundial

--- portafoliofront/src/App.jsx
+++ portafoliofront/src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { useEffect } from 'react'
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import WhatsAppButton from './components/WhatsAppButton'
@@ -11,9 +12,32 @@ import CaseLosPrimos from './pages/CaseLosPrimos'
 import PrivacyPolicy from './pages/PrivacyPolicy'
 import './App.css'
 
+const SITE_ORIGIN = 'https://cristianvilla.com'
+
+function CanonicalSync() {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    const normalized =
+      pathname === '/' ? '/' : pathname.replace(/\/+$/, '') || '/'
+    const href = `${SITE_ORIGIN}${normalized === '/' ? '/' : normalized}`
+
+    let link = document.querySelector('link[rel="canonical"]')
+    if (!link) {
+      link = document.createElement('link')
+      link.setAttribute('rel', 'canonical')
+      document.head.appendChild(link)
+    }
+    link.setAttribute('href', href)
+  }, [pathname])
+
+  return null
+}
+
 function App() {
   return (
     <Router>
+      <CanonicalSync />
       <div className="App">
         <Navbar />
         <main className="main-content">
